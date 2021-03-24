49ea2c8b1523e2255113b4d38ee10194f3cdc888
Devinhodge <d.hodge8891@yahoo.com>
2021-03-24 13:49:14 -0500

    completed

--- src/Router.js
+++ src/Router.js
@@ -5,13 +5,24 @@ import Home from './components/Home'
 import About from './components/About'
 import Car from './components/Car'
 import Login from './components/Login'
+//import { Component } from 'react'
 
 // Write checkAuth function here
 // Check the cookies for a cookie called "loggedIn"
-
+const checkAuth = () => {
+    const cookies = cookie.parse(document.cookie)
+    return cookies['loggedIn'] ? true :  false;
+}
 
 // Write ProtectedRoute function here
-
+const ProtectedRoute = ({ component: Component, ...rest}) => {
+    return (
+    <Route 
+    {...rest}
+    render = {(props) => checkAuth() ? <Component {...props}/> : <Redirect to= '/login'/>}
+    />
+)
+}
 
 const Router = () => {
     return (
